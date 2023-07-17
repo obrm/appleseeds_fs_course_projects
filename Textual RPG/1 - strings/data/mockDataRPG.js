e717eb130128b5abc58eebca56bcfc590b6f5cbd
Ori Baram <obrm770@gmail.com>
2023-07-17 12:36:59 +0300

    strings

---
+++ Textual RPG/1 - strings/data/mockDataRPG.js
@@ -0,0 +1,37 @@
+// Relevant to string exercises 3, 4, 5, 6
+let gameData = {
+  player: {
+    name: "Hero",
+    health: 100,
+    level: 1,
+    location: "forest",
+    inventory: ["sword", "health potion"],
+  },
+  monsters: [
+    {
+      name: "Goblin",
+      health: 25,
+      location: "forest",
+    },
+    {
+      name: "Ogre",
+      health: 50,
+      location: "mountain",
+    },
+    {
+      name: "Dragon",
+      health: 100,
+      location: "castle",
+    },
+    {
+      name: "Witch",
+      health: 30,
+      location: "swamp",
+    },
+    {
+      name: "Ghost",
+      health: 15,
+      location: "graveyard",
+    },
+  ],
+};
---
+++ Travel Planner/data/mockDataTravel.js
@@ -0,0 +1,11 @@
+// Relevant to string exercises 2, 3, 4, 5, 9
+let travelData = {
+  destinations: ["Paris", "London", "New York", "Tokyo", "Sydney"],
+  flights: [
+    { flightNumber: "AA123", destination: "New York", duration: "8h 15m" },
+    { flightNumber: "BA456", destination: "London", duration: "7h 30m" },
+    { flightNumber: "AF789", destination: "Paris", duration: "9h 20m" },
+    { flightNumber: "JL012", destination: "Tokyo", duration: "13h 45m" },
+    { flightNumber: "QF345", destination: "Sydney", duration: "22h 35m" },
+  ],
+};
