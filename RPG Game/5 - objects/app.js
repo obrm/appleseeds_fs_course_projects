8d54fe1cfa7abdbde9652b70cd1f013c82515f51
Ori Baram <obrm770@gmail.com>
2023-07-27 13:12:04 +0300

    objects

--- RPG Game/5 - objects/app.js
+++ RPG Game/5 - objects/app.js
@@ -73,67 +73,69 @@ const gameWorld = {
    * @param {Object} player - The player object
    * @param {Object} npc - The NPC object
    */
-  talkToNPC: function (player, npc) { }
+  talkToNPC: function (player, npc) { },
+
+
+  /**
+   * Create a new game entity
+   * @param {string} name - The name of the entity
+   * @param {number} health - The health of the entity
+   * @param {Object} position - The position of the entity
+   * @param {Array} inventory - The inventory of the entity
+   * @returns {Object} The new entity
+   */
+  createEntity: function (name, health, position, inventory = []) { },
+
+  /**
+   * Create a new item
+   * @param {string} name - The name of the item
+   * @param {Object} effect - The effect of the item
+   * @returns {Object} The new item
+   */
+  createItem: function (name, effect) { },
+
+  /**
+   * Create a new ability
+   * @param {string} name - The name of the ability
+   * @param {Object} effect - The effect of the ability
+   * @returns {Object} The new ability
+   */
+  createAbility: function (name, effect) { },
+
+  /**
+   * Create a new player
+   * @param {string} name - The name of the player
+   * @param {number} health - The health of the player
+   * @param {Object} position - The position of the player
+   * @param {Array} inventory - The inventory of the player
+   * @param {Array} abilities - The abilities of the player
+   * @param {number} level - The level of the player
+   * @param {number} xp - The xp of the player
+   * @param {string} playerClass - The class of the player
+   * @returns {Object} The new player
+   */
+  createPlayer: function (name, health, position, inventory, abilities, level, xp, playerClass) { },
+
+  /**
+   * Create a new enemy
+   * @param {string} name - The name of the enemy
+   * @param {number} health - The health of the enemy
+   * @param {Object} position - The position of the enemy
+   * @param {Array} inventory - The inventory of the enemy
+   * @param {Array} abilities - The abilities of the enemy
+   * @returns {Object} The new enemy
+   */
+  createEnemy: function (name, health, position, inventory, abilities) { },
+
+  /**
+   * Create a new NPC
+   * @param {string} name - The name of the NPC
+   * @param {number} health - The health of the NPC
+   * @param {Object} position - The position of the NPC
+   * @param {Array} inventory - The inventory of the NPC
+   * @param {string} dialog - The dialog of the NPC
+   * @returns {Object} The new NPC
+   */
+  createNPC: function (name, health, position, inventory, dialog) { },
+
 };
-
-/**
- * Create a new game entity
- * @param {string} name - The name of the entity
- * @param {number} health - The health of the entity
- * @param {Object} position - The position of the entity
- * @param {Array} inventory - The inventory of the entity
- * @returns {Object} The new entity
- */
-const createEntity = function (name, health, position, inventory = []) { };
-
-/**
- * Create a new item
- * @param {string} name - The name of the item
- * @param {Object} effect - The effect of the item
- * @returns {Object} The new item
- */
-const createItem = function (name, effect) { };
-
-/**
- * Create a new ability
- * @param {string} name - The name of the ability
- * @param {Object} effect - The effect of the ability
- * @returns {Object} The new ability
- */
-const createAbility = function (name, effect) { };
-
-/**
- * Create a new player
- * @param {string} name - The name of the player
- * @param {number} health - The health of the player
- * @param {Object} position - The position of the player
- * @param {Array} inventory - The inventory of the player
- * @param {Array} abilities - The abilities of the player
- * @param {number} level - The level of the player
- * @param {number} xp - The xp of the player
- * @param {string} playerClass - The class of the player
- * @returns {Object} The new player
- */
-const createPlayer = function (name, health, position, inventory, abilities, level, xp, playerClass) { };
-
-/**
- * Create a new enemy
- * @param {string} name - The name of the enemy
- * @param {number} health - The health of the enemy
- * @param {Object} position - The position of the enemy
- * @param {Array} inventory - The inventory of the enemy
- * @param {Array} abilities - The abilities of the enemy
- * @returns {Object} The new enemy
- */
-const createEnemy = function (name, health, position, inventory, abilities) { };
-
-/**
- * Create a new NPC
- * @param {string} name - The name of the NPC
- * @param {number} health - The health of the NPC
- * @param {Object} position - The position of the NPC
- * @param {Array} inventory - The inventory of the NPC
- * @param {string} dialog - The dialog of the NPC
- * @returns {Object} The new NPC
- */
-const createNPC = function (name, health, position, inventory, dialog) { };
