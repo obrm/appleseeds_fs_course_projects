40e25188e60b2f4503eb358fca9d3643908f6887
Ori Baram <obrm770@gmail.com>
2023-07-30 13:14:28 +0300

    remove music player

--- Music Player/2 - arrow functions/data/musicData.js
+++
@@ -1,7 +0,0 @@
-const songs = [
-  { title: 'Song 1', artist: 'Artist 1', duration: 3.4, genre: 'Rock' },
-  { title: 'Song 2', artist: 'Artist 2', duration: 2.9, genre: 'Pop' },
-  { title: 'Song 3', artist: 'Artist 3', duration: 4.2, genre: 'Jazz' },
-  { title: 'Song 4', artist: 'Artist 4', duration: 3.6, genre: 'Rock' },
-  { title: 'Song 5', artist: 'Artist 5', duration: 3.1, genre: 'Pop' }
-];
