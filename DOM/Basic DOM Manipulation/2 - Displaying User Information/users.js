db23e2ba5e9b4e4b966556c38a30f39ebcc15e12
Ori Baram <obrm770@gmail.com>
2023-08-10 11:20:32 +0300

    DOM

---
+++ DOM/Basic DOM Manipulation/2 - Displaying User Information/users.js
@@ -0,0 +1,17 @@
+const users = [
+  {
+    id: 167464,
+    firstName: 'Jimmy',
+    lastName: 'Arnold',
+  },
+  {
+    id: 578447,
+    firstName: 'Martha',
+    lastName: 'Goldman',
+  },
+  {
+    id: 864578,
+    firstName: 'Tim',
+    lastName: 'Burton',
+  },
+];
